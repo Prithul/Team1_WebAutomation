93063d8436413c04b77abdf8bf7776200f00669e
Prithul <prithul28@gmail.com>
2019-01-10 22:43:40 -0500

    Adds getPageTitle()

--- Amazon/src/test/java/tests/TestHomeAmazon.java
+++ Amazon/src/test/java/tests/TestHomeAmazon.java
@@ -5,10 +5,17 @@ import org.testng.annotations.Test;
 
 public class TestHomeAmazon extends CommonAPI {
 
+
+    @Test
+    public void test1() {
+
+        getCurrentURL();
+    }
+
     @Test
     public void test2() {
 
-        getCurrentURL();
+        getPageTitle();
     }
 
 }
