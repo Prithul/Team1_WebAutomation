7e11d22d49ffa148244d46cd4e3107aa087b985c
Prithul <prithul28@gmail.com>
2019-01-10 22:42:36 -0500

    Adds TestSignInSignOutAmazon for Sign In/Out

---
+++ Amazon/src/test/java/tests/TestSignInSignOutAmazon.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class TestSignInSignOutAmazon extends essentialUtility.SignInSignOut {
+
+    @Test
+    public void test3() throws InterruptedException {
+
+        getSignIn_SignOut();
+    }
+}
