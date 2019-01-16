ab48fc987020ca720d7a76699a8c45118abe127a
Halima <dewanbabee@yahoo.ca>
2019-01-16 14:50:52 -0500

    testing searchPage continues

--- CNN/src/main/java/pages/SearchPage.java
+++ CNN/src/main/java/pages/SearchPage.java
@@ -13,31 +13,36 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SearchPage extends HomePage{
+public class SearchPage extends HomePage {
 
-//    @FindBy(how = How.XPATH, using = "/html/body/div[5]/div[3]/div/div[1]/div/div[1]/button[2]")
+    //    @FindBy(how = How.XPATH, using = "/html/body/div[5]/div[3]/div/div[1]/div/div[1]/button[2]")
 //    public static WebElement searchPageSearchButton;
     @FindBy(how = How.CLASS_NAME, using = "cnn-search__input")
     public static WebElement searchPageSearchTextBox;
 
-    @FindBy(how = How.XPATH, using ="/html/body/div[5]/div[3]/div/div[1]/div/div[1]/button[2]")
+    @FindBy(how = How.XPATH, using = "/html/body/div[5]/div[3]/div/div[1]/div/div[1]/button[2]")
     public static WebElement searchPageSubmitButton;
-    @FindBy(how=How.CLASS_NAME,using = "cnn-search__clear")
+    @FindBy(how = How.CLASS_NAME, using = "cnn-search__clear")
     public WebElement clearButton;
 
 
-
-
-    public void searchFor(String value){
+    public void searchFor(String value) {
         getSearchInputField().sendKeys(value);
     }
-    public void submitSearchButton(){
+
+    public void searchForAndEnter(String value) {
+        getSearchInputField().sendKeys(value, Keys.ENTER);
+    }
+
+    public void submitSearchButton() {
         searchPageSubmitButton.click();
     }
-    public void clearInput(){
+
+    public void clearInput() {
         //driver.findElement(By.className("cnn-search__clear")).click();
         clearButton.click();
     }
+
     public WebElement getSearchInputField() {
         return searchTextBox;
     }
@@ -45,10 +50,11 @@ public class SearchPage extends HomePage{
     public void setSearchInputField(WebElement searchInputField) {
         this.searchTextBox = searchInputField;
     }
-    public void homeSearchItemsAndSubmitButton()throws IOException,InterruptedException {
-       searchButton.click();
+
+    public void homeSearchItemsAndSubmitButton() throws IOException, InterruptedException {
+        searchButton.click();
         List<String> list = getItemValue();
-        for(int i=0; i<list.size(); i++) {
+        for (int i = 0; i < list.size(); i++) {
             searchFor(list.get(i));
             searchSubmitButton.submit();
             sleepFor(5);
@@ -57,14 +63,41 @@ public class SearchPage extends HomePage{
         }
     }
 
-    public void searchPageSearchItemsAndSubmitButton()throws IOException,InterruptedException {
+    public void searchPageSearchItemsAndSubmitButton() throws IOException, InterruptedException {
         List<String> list = getItemValue();
         searchButton.click();
         searchFor(list.get(0));
-        searchSubmitButton.submit();
+        submitSearchButton();
         clearInput();
-        for(int i=1; i<list.size(); i++) {
-          searchPageSearchTextBox.sendKeys(list.get(i));
+        for (int i = 1; i < list.size(); i++) {
+            searchPageSearchTextBox.sendKeys(list.get(i));
+            submitSearchButton();
+            sleepFor(5);
+            clearInput();
+
+
+        }
+    }
+
+
+    public void homeSearchItemsAndEnter() throws InterruptedException {
+        searchButton.click();
+        List<String> list = getItemValue();
+        for (int i = 0; i < list.size(); i++) {
+            searchForAndEnter(list.get(i));
+            sleepFor(5);
+            goCnn();
+        }
+    }
+
+    public void searchPageSearchItemsAndEnter() throws InterruptedException {
+
+        List<String> list = getItemValue();
+        searchButton.click();
+        searchForAndEnter(list.get(0));
+        clearInput();
+        for (int i = 1; i < list.size(); i++) {
+            searchPageSearchTextBox.sendKeys(list.get(i), Keys.ENTER);
             submitSearchButton();
             sleepFor(5);
             clearInput();
@@ -75,27 +108,19 @@ public class SearchPage extends HomePage{
 
 
 
+        public List<String> getItemValue()
+        {
+            List<String> itemsList = new ArrayList<String>();
+            itemsList.add("US");
+            itemsList.add("Politics");
+            itemsList.add("World");
+            itemsList.add("Tech");
+            itemsList.add("Entertainment");
+            itemsList.add("Business");
+            itemsList.add("Travel");
+            itemsList.add("Style");
 
-
-    public void searchItemByEnter()throws InterruptedException{
-        List<String> itemList = getItemValue();
-        for(String st: itemList) {
-            getSearchInputField().sendKeys(st, Keys.ENTER);
-            getSearchInputField().clear();
+            return itemsList;
         }
     }
 
-    public List<String> getItemValue(){
-        List<String> itemsList = new ArrayList<String>();
-        itemsList.add("US");
-        itemsList.add("Politics");
-        itemsList.add("World");
-        itemsList.add("Tech");
-        itemsList.add("Entertainment");
-        itemsList.add("Business");
-        itemsList.add("Travel");
-        itemsList.add("Style");
-
-        return itemsList;
-    }
-}
