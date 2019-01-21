eb8c56abcafc23ea7db33648a21d39f3895c8ea1
Prithul <prithul28@gmail.com>
2019-01-20 20:01:15 -0500

    Testing Rest Assured...

---
+++ RestAssuredRestAPI/src/main/java/getRequest/GetData.java
@@ -0,0 +1,22 @@
+package getRequest;
+
+import org.testng.Assert;
+import static io.restassured.RestAssured.*;
+import io.restassured.response.Response;
+
+    public class GetData{
+
+        public void responseCode(){
+
+            int code = get("https://samples.openweathermap.org/data/2.5/weather?zip=08016,us&appid=b6907d289e10d714a6e88b30761fae22").getStatusCode();
+            System.out.println("Status code: " + code);
+            Assert.assertEquals(code,200);
+        }
+        public void body(){
+
+            String data = get("https://samples.openweathermap.org/data/2.5/weather?zip=08016,us&appid=b6907d289e10d714a6e88b30761fae22").asString();
+            long time = get("https://samples.openweathermap.org/data/2.5/weather?zip=08016,us&appid=b6907d289e10d714a6e88b30761fae22").getTime();
+            System.out.println("Response Time: " + time);
+        }
+    }
+
