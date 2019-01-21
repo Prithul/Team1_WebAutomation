d98feae889e59306ec02f912f5b02747fd51bb1d
Halima <dewanbabee@yahoo.ca>
2019-01-21 03:28:05 -0500

    CNN Tvpage

--- CNN/src/main/java/pages/LiveTvPage.java
+++ CNN/src/main/java/pages/LiveTvPage.java
@@ -1,4 +1,19 @@
 package pages;
 
-public class LiveTvPage {
+import base.CommonAPI;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+public class LiveTvPage extends CommonAPI {
+    @FindBy(how = How.CSS,using="")
+    public WebElement tvPageTitle;
+//    @FindBy(how=How.CSS,using = "")
+//    public WebElement
+
+
+
+
+    int size = driver.findElements(By.tagName("iframe")).size();
 }
