9300c5b4a3cc6c1d721fdc31c50ccbccef322a60
Halima <dewanbabee@yahoo.ca>
2019-01-23 02:25:07 -0500

    LiveTvPage

--- CNN/src/main/java/pages/LiveTvPage.java
+++ CNN/src/main/java/pages/LiveTvPage.java
@@ -6,14 +6,123 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
+import java.util.List;
+
 public class LiveTvPage extends CommonAPI {
-    @FindBy(how = How.CSS,using="")
-    public WebElement tvPageTitle;
-//    @FindBy(how=How.CSS,using = "")
-//    public WebElement
+    @FindBy(how = How.CSS,using="#nav-mobileTV")
+    public WebElement liveTvPageIcon;
+    @FindBy(how =How.CSS,using = "#mvpdpicker > div.header > div.close")
+    public WebElement crossButton;
+    @FindBy(how = How.CSS,using ="#mvpdpicker > div.header > div.title > span")
+    public WebElement logInButton;
+    @FindBy(how = How.CSS,using ="#mvpdpicker > div.header > div.help")
+    public WebElement helpButton;
+    @FindBy(how = How.CSS,using ="#mvpdpicker > div.slates > div.slate.pickbylogo > ul")
+    public List<WebElement> listOfProviders;
 
 
+public void checkLiveTv()
+{
+    liveTvPageIcon.click();
+}
+    public String checkLiveTvTitle()
+    {
+        liveTvPageIcon.click();
+        return driver.getTitle();
+    }
+    public String checkLiveTvUrl()
+    {
+        liveTvPageIcon.click();
+        for (String handle : driver.getWindowHandles()) {
 
+            driver.switchTo().window(handle);
+        }
+        return driver.getCurrentUrl();
 
-    int size = driver.findElements(By.tagName("iframe")).size();
+    }
+
+    public void checkLiveTvClose()
+    {
+        liveTvPageIcon.click();
+        for (String handle : driver.getWindowHandles()) {
+
+            driver.switchTo().window(handle);
+        }
+        driver.getCurrentUrl();
+        crossButton.click();
+
+    }
+
+    public String checkLiveTvCloseUrl()
+    {
+        String parentWindowHandler = driver.getWindowHandle();
+        liveTvPageIcon.click();
+
+        crossButton.click();
+        for (String handle : driver.getWindowHandles()) {
+
+            driver.switchTo().window(handle);
+        }
+
+        return driver.getCurrentUrl();
+
+    }
+
+    public String checkLoginLabel()
+    {
+        liveTvPageIcon.click();
+        for (String handle : driver.getWindowHandles()) {
+
+            driver.switchTo().window(handle);
+        }
+        return logInButton.getText();
+    }
+    public void checkHelpClick()
+    {
+        liveTvPageIcon.click();
+        for (String handle : driver.getWindowHandles()) {
+
+            driver.switchTo().window(handle);
+        }
+        helpButton.click();
+    }
+
+    public String checkHelpUrl()
+    {
+        liveTvPageIcon.click();
+        helpButton.click();
+        for (String handle : driver.getWindowHandles()) {
+
+            driver.switchTo().window(handle);
+        }
+      return driver.getCurrentUrl();
+
+    }
+
+    public void checkListOfProviders()
+    {
+        liveTvPageIcon.click();
+        List<WebElement> list = listOfProviders;
+        for(WebElement provider :list)
+        {
+            provider.click();
+        }
+    }
+    public String checkListOfProvidersUrl(int urlNo)
+    {
+        liveTvPageIcon.click();
+        List<WebElement> list = listOfProviders;
+        for(int i =urlNo;i<list.size();i++)
+        {
+            list.get(i).click();
+            break;
+        }
+        for (String handle : driver.getWindowHandles()) {
+
+            driver.switchTo().window(handle);
+        }
+
+        return driver.getCurrentUrl();
+    }
+
 }
--- CNN/src/test/java/tests/LiveTvPageTest.java
+++ CNN/src/test/java/tests/LiveTvPageTest.java
@@ -1,4 +1,77 @@
 package tests;
 
-public class LiveTvPageTest {
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LiveTvPage;
+
+public class LiveTvPageTest extends LiveTvPage{
+//    @Test
+//    public void testCheckLiveTv()
+//    {
+//        LiveTvPage liveTv = new PageFactory().initElements(driver, LiveTvPage.class);
+//        liveTv.checkLiveTv();
+//    }
+
+//    @Test
+//    public void testCheckLiveTvTitle()
+//    {
+//        LiveTvPage liveTv = new PageFactory().initElements(driver, LiveTvPage.class);
+//        Assert.assertEquals(liveTv.checkLiveTvTitle(),"CNNgo - CNN.com");
+//    }
+//@Test
+//public void testCheckLiveTvUrl()
+//{
+//    LiveTvPage liveTv = new PageFactory().initElements(driver, LiveTvPage.class);
+//    Assert.assertEquals(liveTv.checkLiveTvUrl(),"https://go.cnn.com/?stream=cnn%3Fsr");
+//}
+//    @Test
+//    public void testCheckLiveTvCross()
+//    {
+//        LiveTvPage liveTv = new PageFactory().initElements(driver, LiveTvPage.class);
+//        liveTv.checkLiveTvClose();
+//    }
+//    @Test
+//    public void testCheckLiveTvCrossUrl()
+//    {
+//        LiveTvPage liveTv = new PageFactory().initElements(driver, LiveTvPage.class);
+//        Assert.assertEquals(liveTv.checkLiveTvCloseUrl(),"https://www.cnn.com/tv");
+//    }
+//    @Test
+//    public void testCheckLiveTvLoginLabel()
+//    {
+//        LiveTvPage liveTv = new PageFactory().initElements(driver, LiveTvPage.class);
+//        Assert.assertEquals(liveTv.checkLoginLabel(),"Login");
+//    }
+//    @Test
+//    public void testCheckLiveTvHelpLabel()
+//    {
+//        LiveTvPage liveTv = new PageFactory().initElements(driver, LiveTvPage.class);
+//       liveTv.checkHelpClick();
+//    }
+
+//    @Test
+//    public void testCheckLiveTvHelpUrl()
+//    {
+//        LiveTvPage liveTv = new PageFactory().initElements(driver, LiveTvPage.class);
+//        Assert.assertEquals(liveTv.checkHelpUrl(),"https://www.cnn.com/about");
+//
+//    }
+
+//    @Test
+//    public void testCheckListOfProviders()
+//    {
+//        LiveTvPage liveTv = new PageFactory().initElements(driver, LiveTvPage.class);
+//        //Assert.assertEquals(liveTv.checkHelpUrl(),"https://www.cnn.com/about");
+//        liveTv.checkListOfProviders();
+//    }
+
+    @Test
+    public void testCheckListOfProvidersUrl1()
+    {
+        LiveTvPage liveTv = new PageFactory().initElements(driver, LiveTvPage.class);
+        Assert.assertEquals(liveTv.checkListOfProvidersUrl(0),"https://loginprodx.att.net/commonLogin/igate_edam/controller.do?TAM_OP=login&USERNAME=unauthenticated&ERROR_CODE=0x00000000&ERROR_TEXT=HPDBA0521I%20%20%20Successful%20completion&METHOD=GET&URL=%2FFIM%2Fsps%2Fauth%3FFedName%3DATT-ADB%26FedId%3Duuid2b03a1d7-0139-1cca-be40-d32385710be9%3Ftucd567%3Dw&REFERER=https%3A%2F%2Fsp.auth.adobe.com%2Fadobe-services%2Fauthenticate%2Fsaml%3Fnoflash%3Dtrue%26mso_id%3DATT%26requestor_id%3DCNN%26no_iframe%3Dtrue%26domain_name%3Dadobe.com%26redirect_url%3Dhttps%253A%252F%252Fentitlement.auth.adobe.com%252Fentitlement%252Fjs%252FcompleteBackgroundLogin.html&HOSTNAME=loginprodx.att.net&AUTHNLEVEL=&FAILREASON=&PROTOCOL=https&OLDSESSION=");
+        //liveTv.checkListOfProvidersUrl(0);
+    }
 }
