39d1424217d33ca38c7bc9494ee3c77566df0b15
Halima <dewanbabee@yahoo.ca>
2019-01-24 03:19:32 -0500

    DDT

--- CNN/src/test/java/datadriven/ExcelReaderTest.java
+++ CNN/src/test/java/datadriven/ExcelReaderTest.java
@@ -1,4 +1,84 @@
 package datadriven;
 
-public class ExcelReaderTest {
+import cnnutility.ExcelReader;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.testng.annotations.Test;
+import reporting.TestLogger;
+
+import java.io.IOException;
+
+public class ExcelReaderTest extends ExcelReader {
+//    @Test
+//    public void testNavItemfromExcel0() throws InvalidFormatException, IOException ,InterruptedException{
+//        TestLogger.log("Browser is launched");
+//        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {}.getClass().getEnclosingMethod().getName()));
+//        readNavItemExcel(0,0);
+//    }
+//    @Test
+//    public void testNavItemfromExcel1() throws InvalidFormatException, IOException ,InterruptedException{
+//        TestLogger.log("Browser is launched");
+//        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {}.getClass().getEnclosingMethod().getName()));
+//        readNavItemExcel(1,1);
+//    }
+//    @Test
+//    public void testNavItemfromExcel2() throws InvalidFormatException, IOException ,InterruptedException{
+//        TestLogger.log("Browser is launched");
+//        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {}.getClass().getEnclosingMethod().getName()));
+//        readNavItemExcel(2,2);
+//    }
+//    @Test
+//    public void testNavItemfromExcel3() throws InvalidFormatException, IOException ,InterruptedException{
+//        TestLogger.log("Browser is launched");
+//        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {}.getClass().getEnclosingMethod().getName()));
+//        readNavItemExcel(3,3);
+//    }
+//    @Test
+//    public void testNavItemfromExcel4() throws InvalidFormatException, IOException ,InterruptedException {
+//        TestLogger.log("Browser is launched");
+//        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+//        }.getClass().getEnclosingMethod().getName()));
+//        readNavItemExcel(4, 4);
+//    }
+//    @Test
+//    public void testNavItemfromExcel5() throws InvalidFormatException, IOException ,InterruptedException {
+//        TestLogger.log("Browser is launched");
+//        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+//        }.getClass().getEnclosingMethod().getName()));
+//        readNavItemExcel(5, 5);
+//    }
+//    @Test
+//    public void testNavItemfromExcel6() throws InvalidFormatException, IOException ,InterruptedException {
+//        TestLogger.log("Browser is launched");
+//        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+//        }.getClass().getEnclosingMethod().getName()));
+//        readNavItemExcel(6, 6);
+//    }
+//    @Test
+//    public void testNavItemfromExcel7() throws InvalidFormatException, IOException ,InterruptedException {
+//        TestLogger.log("Browser is launched");
+//        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+//        }.getClass().getEnclosingMethod().getName()));
+//        readNavItemExcel(7, 7);
+//    }
+//    @Test
+//    public void testNavItemfromExcel8() throws InvalidFormatException, IOException ,InterruptedException {
+//        TestLogger.log("Browser is launched");
+//        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+//        }.getClass().getEnclosingMethod().getName()));
+//        readNavItemExcel(8, 8);
+//    }
+    @Test
+    public void testNavItemfromExcel9() throws InvalidFormatException, IOException ,InterruptedException {
+        TestLogger.log("Browser is launched");
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        readNavItemExcel(9, 9);
+    }
+//    @Test
+//    public void testNavItemfromExcel10() throws InvalidFormatException, IOException ,InterruptedException {
+//        TestLogger.log("Browser is launched");
+//        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+//        }.getClass().getEnclosingMethod().getName()));
+//        readNavItemExcel(10, 10);
+//    }
 }
